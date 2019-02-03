c9ac35adc11e6d93b6e1e5480dbd2fb1239a3821
Bryn Bennett <bryn.bennett@gmail.com>
2019-02-03 23:37:03 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -5,11 +5,11 @@
 my_songs = {
  "Go Go GO" => 'home/bbennett7/jukebox-cli-online-web-prework/jukebox-cli/audio/Emerald-Park/01.mp3',
  "LiberTeens" => 'home/bbennett7/jukebox-cli-online-web-prework/jukebox-cli/audio/Emerald-Park/02.mp3',
- "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
- "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
- "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
- "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
- "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
+ "Hamburg" =>  'home/bbennett7/jukebox-cli-online-web-prework/jukebox-cli/audio/Emerald-Park/03.mp3',
+ "Guiding Light" => 'home/bbennett7/jukebox-cli-online-web-prework/jukebox-cli/audio/Emerald-Park/04.mp3',
+ "Wolf" => '<home/bbennett7/jukebox-cli-online-web-prework/jukebox-cli/audio/Emerald-Park/05.mp3',
+ "Blue" => 'home/bbennett7/jukebox-cli-online-web-prework/jukebox-cli/audio/Emerald-Park/06.mp3',
+ "Graduation Failed" => 'home/bbennett7/jukebox-cli-online-web-prework/jukebox-cli/audio/Emerald-Park/07.mp3'
  }
 
 def help
